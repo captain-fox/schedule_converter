1b183ef74801482475df9c9b2c3e9c1e3d9496dd
Stanislaw Reduta <stanislaw.reduta@gmail.com>
2017-01-05 04:06:48 +0100

    Making minor changes in method names and code refactoring

--- group_info.py
+++ group_info.py
@@ -1,3 +1,14 @@
+def get_groups(groups, lines):
+    for row in lines:
+        if row[12] in groups:
+            continue
+        elif row[12] is '':
+            continue
+        else:
+            groups.append(row[12])
+    return groups
+
+
 def collect_group_info(lines, user_choice):
     for row in lines:
         if row[12] == user_choice:
--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 import csv
-import file_loader, group_info, get_groups
+import file_manager, group_info, get_groups
 
-filename = file_loader.file_loader()
-
+filename = file_manager.search_for_file()
 lines = []
 groups = []
 
+with open(filename, 'rt', encoding='windows 1250') as csv_input:
 
-
-with open(filename, 'rt', encoding='windows 1250') as fileinput:
-
-    reader = csv.reader(fileinput, delimiter=';')
+    reader = csv.reader(csv_input, delimiter=';')
 
     for row in reader:
         lines.append(row)
 
-    #filling up the list of groups
-groups = get_groups.get_groups(groups, lines)
+#filling up the list of groups
+groups = group_info.get_groups(groups, lines)
 
-    # Just to make sure there's something in 'groups'
-    # print(groups[1:])
+# print(groups[1:])
 
-    # user_choice = input('Group?')
+# user_choice = input('Group?')
 
-    # setting up user_choice to make things simpler for testing
+# setting up user_choice to make things simpler for testing
 user_choice = '5I'
 group_info.collect_group_info(lines, user_choice)
 
