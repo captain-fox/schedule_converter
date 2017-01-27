0b3893a01cc8d8960147e16680a9449cae42bb3f
Stanislaw Reduta <stanislaw.reduta@gmail.com>
2017-01-27 16:32:35 +0100

    Refactoring, no changes to logic of methods

--- time_keeper.py
+++ time_keeper.py
@@ -10,12 +10,14 @@ def set_semester():
     start_date = date(2016, 9, 26)
     # end_date = date(int(input('Semester ends\nYear: ')), int(input('Month: ')), int(input('Day: ')))
     end_date = date(2017, 1, 25)
+    holidays_start = date(2016, 12, 22)
+    holidays_end = date(2017, 1, 2)
     # print('Semester starts on',weekdays[calendar.weekday(start_date[0], start_date[1], start_date[2])])
     print('Semester starts on', weekdays[start_date.weekday()], start_date)
     # print('Semester ends on', weekdays[calendar.weekday(end_date[0], end_date[1], end_date[2])])
     print('Semester ends on', weekdays[end_date.weekday()], end_date)
-    diff = end_date - start_date
-    print('Semester is ', diff.days , 'long.\n')
+    delta = end_date - start_date
+    print('Semester is', delta.days, 'days long.\n')
 
     x = start_date
     weeknumber = 1
@@ -24,8 +26,7 @@ def set_semester():
         x += + timedelta(days=1)
         if x.weekday() == 0:
             weeknumber += 1
-            print('\n')
-            continue
+            print('')
 
 
 # retrieves current date/time
