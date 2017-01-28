96f4a7b92b0553996a3685ebf36bb210692ce434
Stanislaw Reduta <stanislaw.reduta@gmail.com>
2017-01-28 17:41:08 +0100

    Changing divide_by_weeks method to understand and skip holidays.

--- time_keeper.py
+++ time_keeper.py
@@ -1,48 +1,51 @@
 from datetime import *
-import calendar
-calendar.setfirstweekday(calendar.MONDAY)
 
 weekdays = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
 
 
-def set_semester():
+def set_term():
     # start_date = date(int(input('Semester begins\nYear: ')), int(input('Month: ')), int(input('Day: ')))
-    start_date = date(2016, 9, 26)
+    term_start = date(2016, 9, 26)
     # end_date = date(int(input('Semester ends\nYear: ')), int(input('Month: ')), int(input('Day: ')))
-    end_date = date(2017, 1, 25)
+    term_end = date(2017, 1, 25)
     holidays_start = date(2016, 12, 22)
     holidays_end = date(2017, 1, 2)
     # print('Semester starts on',weekdays[calendar.weekday(start_date[0], start_date[1], start_date[2])])
-    print('Semester starts on', weekdays[start_date.weekday()], start_date)
+    print('\nSemester starts on', weekdays[term_start.weekday()], term_start)
+    print('\nHolidays start on', weekdays[holidays_start.weekday()], holidays_start)
+    print('Holidays end on', weekdays[holidays_end.weekday()], holidays_end)
     # print('Semester ends on', weekdays[calendar.weekday(end_date[0], end_date[1], end_date[2])])
-    print('Semester ends on', weekdays[end_date.weekday()], end_date)
-    delta = end_date - start_date
+    print('\nSemester ends on', weekdays[term_end.weekday()], term_end)
+    delta = term_end - term_start
     print('Semester is', delta.days, 'days long.')
-    term = [start_date,end_date]
-    return term
+    return [term_start, term_end, holidays_start, holidays_end]
 
 
-def divide_by_weeks(start_date, end_date):
-    x = start_date
-    weeknumber = 0
-    while x <= end_date:
+def divide_by_weeks(term_start, term_end, hol_start, hol_end):
+    i = term_start
+    week_num = 0
 
-        if x.weekday() == 0:
-            weeknumber += 1
-            print('\nWeek', weeknumber)
+    while i <= term_end:
 
-        if x.weekday() not in [5, 6]:  # skipping weekends
-            print(x, 'is', weekdays[x.weekday()])
-        x += + timedelta(days=1)
+        if i == hol_start:
+            week_num += 1
+            print('\nHolidays\n')
+            print('Week', week_num)
+            i = hol_end + timedelta(days=1)
+            continue
 
+        if i.weekday() == 0:
+            week_num += 1
+            print('\nWeek', week_num)
+
+        if i.weekday() not in [5, 6]:  # skipping weekends
+            print(i, 'is', weekdays[i.weekday()])
+        i += timedelta(days=1)
 
 
 # retrieves current date/time
 # today = datetime.today()
 # print(datetime.weekday(today))
 
-
-set_semester()
-
-term = set_semester()
-divide_by_weeks(term[0], term[1])
+term = set_term()
+divide_by_weeks(term[0], term[1], term[2], term[3])
