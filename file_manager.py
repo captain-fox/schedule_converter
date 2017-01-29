e7bdcc04d338b0e51cb607a62527793303a17e3e
Stanislaw Reduta <stanislaw.reduta@gmail.com>
2017-01-29 22:52:31 +0100

    Adds 'preview_ics_output' method to 'file_manager.py' to make possible to preview expected output for .ics file

--- file_manager.py
+++ file_manager.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import time_keeper
 
 os.chdir(".")
 
@@ -84,4 +85,54 @@ def add_to_existing_ics(filename, class_title, week_day, start_time, end_time, w
     output_file.close()
 
 
+def preview_ics_output(term, gathered_data):
+
+    term_start = term[0]
+    term_end = term[1]
+    holidays_start = term[2]
+    holidays_end = term[3]
+
+    class_title = gathered_data[0]
+    week_day_index = gathered_data[1]
+    start_time = gathered_data[2]
+    end_time = gathered_data[3]
+    class_type = gathered_data[4]
+    location = gathered_data[5]
+    lecturer = gathered_data[6]
+
+    i = term_start
+    week_num = 0
+
+    while i <= term_end:
+
+        if i == holidays_start:
+            week_num += 1
+            print('Holidays\n')
+            # print('Week', week_num)
+            i = holidays_end + time_keeper.timedelta(days=1)
+            continue
+
+        if i.weekday() == 0:
+            week_num += 1
+
+        if i.weekday() == week_day_index:
+            print('BEGIN:VEVENT')
+            print('DTSTART:' + i.strftime('%Y%m%d') + 'T' + start_time + '00Z')
+            print('DTEND:' + i.strftime('%Y%m%d') + 'T' + end_time + '00Z')
+            # output_file.write('RRULE:FREQ=WEEKLY;UNTIL=20170217T000000Z\n')
+            print('SUMMARY:' + class_title)
+            print('DESCRIPTION:Prowadzący: ' + lecturer)
+            print('LOCATION:' + location)
+            print('TRANSP:OPAQUE')
+            print('END:VEVENT\n')
+
+            # print('Week', week_num)
+            # print('Class:', class_title)
+            # print(i.strftime('%Y%m%d'), 'Day of the week:', i.weekday())
+            # print('Start time:', start_time)
+            # print('End time:', end_time)
+            # print('Type of class: ', class_type)
+            # print('Room:', location)
+            # print('Lecturer: ', lecturer, '\n')
+        i += time_keeper.timedelta(days=1)
 
