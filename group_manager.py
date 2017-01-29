c19b12b6546e5baf5fad58b4d92d4992e5cf98e3
Stanislaw Reduta <stanislaw.reduta@gmail.com>
2017-01-29 13:19:35 +0100

    Creates modified version of collect_group_info method to test data gathered.
    collect_group_info method now parses data gathered for single event, passes it to be iterativelly printed for each week day for each event.

--- group_manager.py
+++ group_manager.py
@@ -36,6 +36,7 @@ def parse_data_to_ics(rows, grouptitle):
                 continue
             else:
                 class_title = row[6]
+                # week_day = time_keeper.get_week_day_index(row[1])
                 week_day = row[1]
                 # converting start/end time into "hhmm" format for .ics
                 start_time = row[2][0:2] + row[2][3:5]
@@ -48,7 +49,28 @@ def parse_data_to_ics(rows, grouptitle):
     file_manager.finalise_file(filename)
 
 
-def collect_group_info(rows, user_group):
+def collect_group_info(rows, user_group, term):
+    for row in rows:
+        if row[12] == user_group:
+            # Doesn't print events with empty 'Module' field
+            if row[6] == '':
+                continue
+            else:
+                class_title = row[6]
+                week_day_index = time_keeper.get_week_day_index(row[1])
+                week_day = row[1]
+                start_time = row[2][0:5]
+                end_time = row[2][6:]
+                weeks = row[3]
+                class_type = row[4]
+                location = row[8]
+                lecturer = row[9]
+
+                # print('Day of the week:', time_keeper.get_week_day_index(week_day))
+                time_keeper.create_events_by_day(term[0], term[1], term[2], term[3], class_title, week_day_index, start_time, end_time, class_type, location, lecturer)
+
+
+def test_method_for_groups(rows, user_group):
     for row in rows:
         if row[12] == user_group:
             # Doesn't print events with empty 'Module' field
@@ -63,13 +85,11 @@ def collect_group_info(rows, user_group):
                 class_type = row[4]
                 location = row[8]
                 lecturer = row[9]
+
                 print('Class:', class_title)
                 print('Day of the week:', week_day)
-                print(time_keeper.get_week_day_index(week_day))
                 print('Start time:', start_time)
                 print('End time:', end_time)
-                print('Weeks:', weeks)
                 print('Type of class: ', class_type)
                 print('Room:', location)
                 print('Lecturer: ', lecturer, '\n')
-
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import csv
 import file_manager
 import group_manager
+import time_keeper
 
 # filename = input('Paste name of a file to process\n')
 filename = 'plan.csv'
@@ -9,15 +10,19 @@ rows = file_manager.parse_file(filename)
 
 #creating the list of groups
 groups = group_manager.get_groups(rows)
-# weeks = group_info.get_weeks(lines)
-
 print(groups[1:])
+# weeks = group_info.get_weeks(lines)
 # print(weeks)
+
+# term
+term = time_keeper.set_term()
+time_keeper.show_term_info(term[0], term[1], term[2], term[3])
+
 
 # user_choice = input('Group?')
 # setting up user_choice to make things simpler for testing
 # user_group = '5I'
 user_group = '5I IO2'
-group_manager.collect_group_info(rows, user_group)
+group_manager.collect_group_info(rows, user_group, term)
 # group_info.parse_data_to_ics(lines, user_group)
 
