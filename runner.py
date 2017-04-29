8e8a277715289c031e6e9fea7f566f2f3445d0a5
Stan Reduta <stanislaw.reduta@gmail.com>
2017-04-30 01:04:49 +0200

    Imports to runner everything from model rather than importing model itself.

--- runner.py
+++ runner.py
@@ -1,18 +1,18 @@
-import model
+from model import *
 from _datetime import *
 
 filename = '17l.txt'
-rows = model.FileHandler.read_csv_file(filename)
-model.InputConverter.set_term(date(2016, 9, 26), date(2017, 1, 25), date(2016, 12, 22), date(2017, 1, 2))
-groups = model.Group.get_groups(rows)
+rows = FileHandler.read_csv_file(filename)
+InputConverter.set_term(date(2016, 9, 26), date(2017, 1, 25), date(2016, 12, 22), date(2017, 1, 2))
+groups = Group.get_groups(rows)
 print(groups)
 
 d = date(2017, 1, 2)
 gr = '6I IO-2'
-objects = []
+[]
 
-model.Event.collect_events_for_group(rows, gr)
+Event.collect_events_for_group(rows, gr)
 
-for e in model.Event.EVENTS:
+for e in Event.EVENTS:
     e.preview_ics_output(d)
     print()
