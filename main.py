2657aa58cf7471c3af49234158224f88de91fa19
Stan Reduta <stanislaw.reduta@gmail.com>
2017-05-31 00:51:44 +0200

    Removes draft file.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from model import *
-from term import *
+from Event import *
+from TermHandler import *
 from _datetime import *
 
 
@@ -7,19 +7,19 @@ from _datetime import *
 
 filename = '17l.txt'
 schedule = FileHandler.read_csv_file(filename, InputConverter.__HEADERS__)
-term1 = SummerTerm(date(2016, 9, 26), date(2017, 1, 25), date(2016, 12, 22), date(2017, 1, 2))
-print(term1.return_term_start())
-term2 = WinterTerm(date(2016, 9, 26), date(2017, 1, 25), date(2016, 12, 22), date(2017, 1, 2))
+
+sterm = SummerTerm(date(2017, 2, 27), date(2017, 6, 21), date(2017, 4, 14), date(2017, 4, 18))
+sterm.display_term()
 
 groups = Group.get_groups(schedule)
-# print(groups)
+print(groups)
 
 d = date(2017, 1, 2)
 gr = '6I IO-2'
 objects = []
 
-# Event.collect_events_for_group(schedule, gr)
+Event.collect_events_for_group(schedule, gr)
 
-# for e in Event.EVENTS:
-#     e.preview_ics_output(d)
-#     print()
+for e in Event.EVENTS:
+    e.preview_ics_output(d)
+    print()
