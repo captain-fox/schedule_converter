d924df0c8df9561167b569e33ce2a96ca14e0dfb
Stan Reduta <stanislaw.reduta@gmail.com>
2017-06-04 11:43:39 +0200

    Adds __name__ == __main__ condition and facade method.
    To make sure main does not get called accidentally from other file.

--- main.py
+++ main.py
@@ -4,22 +4,25 @@ from _datetime import *
 
 
 # Facade between GUI and back-end
+def facade():
+    filename = '17l.txt'
+    schedule = FileHandler.read_csv_file(filename, InputConverter.__HEADERS__)
 
-filename = '17l.txt'
-schedule = FileHandler.read_csv_file(filename, InputConverter.__HEADERS__)
+    sterm = SummerTerm(date(2017, 2, 27), date(2017, 6, 21), date(2017, 4, 14), date(2017, 4, 18))
+    sterm.display_term()
 
-sterm = SummerTerm(date(2017, 2, 27), date(2017, 6, 21), date(2017, 4, 14), date(2017, 4, 18))
-sterm.display_term()
+    groups = Group.get_groups(schedule)
+    # print(groups)
 
-groups = Group.get_groups(schedule)
-print(groups)
+    d = date(2017, 1, 2)
+    gr = '6I IO-2'
+    objects = []
 
-d = date(2017, 1, 2)
-gr = '6I IO-2'
-objects = []
+    Event.collect_events_for_group(schedule, gr)
 
-Event.collect_events_for_group(schedule, gr)
+    for e in Event.EVENTS:
+        e.preview_ics_output(d)
+        print()
 
-for e in Event.EVENTS:
-    e.preview_ics_output(d)
-    print()
+if __name__ == '__main__':
+    facade()
