becd9586ff59397ce1fd4912a3670b7557cd81ad
Stan Reduta <stanislaw.reduta@gmail.com>
2017-06-08 11:07:14 +0200

    FileCreator now uses formatter to pass agruments to filewriter.

--- FormattingStrategy.py
+++ FormattingStrategy.py
@@ -1,14 +1,15 @@
 from abc import abstractmethod, ABCMeta
 
+
 class ICSFormatter(ABCMeta):
 
     @classmethod
     def select_processing_method(mcs, filename):
 
         if filename is None:
-            return ConsoleLogger
+            return ConsoleLogger.pass_data
         else:
-            return FileCreator
+            return FileCreator.pass_data
 
     @staticmethod
     @abstractmethod
@@ -21,11 +22,11 @@ class FileCreator(ICSFormatter):
     def pass_data(event, day, file_name=None):
         output_file = open(file_name, 'a')
         output_file.write('BEGIN:VEVENT\n')
-        output_file.write('DTSTART;TZID=' + event.TIME_ZONE + ':' + day.strftime('%Y%m%d') + 'T' + event.start_time + '00\n')
-        output_file.write('DTEND;TZID=' + event.TIME_ZONE + ':' + day.strftime('%Y%m%d') + 'T' + event.end_time + '00\n')
-        output_file.write('SUMMARY:' + event.class_title + '\n')
-        output_file.write('DESCRIPTION: Prowadzący: ' + event.teacher + '\n')
-        output_file.write('LOCATION:' + event.location + '\n')
+        output_file.write('DTSTART;TZID={}:{}T{}00\n'.format(event.TIME_ZONE, day.strftime('%Y%m%d'), event.start_time))
+        output_file.write('DTEND;TZID={}:{}T{}00\n'.format(event.TIME_ZONE, day.strftime('%Y%m%d'), event.end_time))
+        output_file.write('SUMMARY:{}\n'.format(event.class_title))
+        output_file.write('DESCRIPTION: Prowadzący: {}; Rodzaj zajęć: {}\n'.format(event.teacher, event.class_type))
+        output_file.write('LOCATION:{}\n'.format(event.location))
         output_file.write('TRANSP:OPAQUE\n')
         output_file.write('END:VEVENT\n\n')
         output_file.close()
