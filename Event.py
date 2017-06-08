5d7178bbff128db3a8feb64d58584cd5c9ada5f1
Stan Reduta <stanislaw.reduta@gmail.com>
2017-06-08 11:05:40 +0200

    Deletes no longer used methods for writing/consoling event.

--- Event.py
+++ Event.py
@@ -19,31 +19,6 @@ class Event:
         self.location = InputConverter.get_location_from(row)
         self.teacher = InputConverter.get_teacher_from(row)
 
-    def append_to_ics(self, day, file_name=None):
-
-        output_file = open(file_name, 'a')
-        output_file.write('BEGIN:VEVENT\n')
-        output_file.write('DTSTART;TZID=' + self.TIME_ZONE + ':' + day.strftime('%Y%m%d') + 'T' + self.start_time + '00\n')
-        output_file.write('DTEND;TZID=' + self.TIME_ZONE + ':' + day.strftime('%Y%m%d') + 'T' + self.end_time + '00\n')
-        output_file.write('SUMMARY:' + self.class_title + '\n')
-        output_file.write('DESCRIPTION: Prowadzący: ' + self.teacher + '\n')
-        output_file.write('LOCATION:' + self.location + '\n')
-        output_file.write('TRANSP:OPAQUE\n')
-        output_file.write('END:VEVENT\n\n')
-        output_file.close()
-
-    # test method
-    def preview_ics_output(self, day, file_name=None):
-
-        print('BEGIN:VEVENT')
-        print('DTSTART;TZID={}:{}T{}00'.format(self.TIME_ZONE, day.strftime('%Y%m%d'), self.start_time))
-        print('DTEND;TZID={}:{}T{}00'.format(self.TIME_ZONE, day.strftime('%Y%m%d'), self.end_time))
-        print('SUMMARY:{}'.format(self.class_title))
-        print('DESCRIPTION: Prowadzący: {}; Rodzaj zajęć: {}'.format(self.teacher, self.class_type))
-        print('LOCATION:{}'.format(self.location))
-        print('TRANSP:OPAQUE')
-        print('END:VEVENT')
-
     @staticmethod
     def collect_events_for_group(schedule, group):
         try:
