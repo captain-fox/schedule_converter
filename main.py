de0bbe32ea5f03865392edea324bb24f80dc9a5e
Stan Reduta <stanislaw.reduta@gmail.com>
2017-06-20 00:37:24 +0200

    [ROUTINES] Pre-setup for presentation.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from _datetime import *
 from InputConverter import *
 from Group import *
 from OutputHandler import *
+from FileHandler import *
 
 
 # Facade between GUI and back-end
@@ -11,22 +12,22 @@ def facade():
 
     filename = '17l.txt'
     schedule = FileHandler.read_csv_file(filename, InputConverter.__HEADERS__)
-
     term = SummerTerm(date(2017, 2, 27), date(2017, 6, 21), date(2017, 4, 14), date(2017, 4, 18))
-
     groups = Group.get_groups(schedule)
     print(groups)
-
     db = Event.collect_events_for_group(schedule, '6I IO-2')
 
-    # FileCreator.create_ics(group, term)
-    # ConsoleLogger.create_ics(group, term)
+    # # #
 
-    outputfile = None
-    # outputfile = FileHandler.create_and_prepare_file('6I IO-2')
-    for e in db:
-        FileHandler.add_to_existing_ics(e, term, outputfile)
-    # FileHandler.finalise_file(outputfile)
+    outputfile = '6I IO-2'
+    # outputfile = None
+    if outputfile is None:
+        strategy = ConsoleLogger.create_ics
+        outputfile = sys.stdout
+    else:
+        strategy = FileCreator.create_ics
+
+    FileHandler.add_to_existing_ics(strategy, db, term, outputfile)
 
 if __name__ == '__main__':
     facade()
