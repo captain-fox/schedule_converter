7424f15bb2a2fce96502920bbc5cd80f17d8044f
Stan Reduta <stanislaw.reduta@gmail.com>
2017-07-15 14:50:02 +0200

    Creates a class that will handle communication between Django DB and converter.

--- DatabaseHandler.py
+++ DatabaseHandler.py
@@ -1,4 +1,25 @@
+from Event import *
+from TermHandler import *
+from _datetime import *
+from InputConverter import *
+from Group import *
+from OutputHandler import *
+from FileHandler import *
+from DatabaseHandler import *
 
 
-class DataBaseBridge:
-    pass
+class DatabaseBridge:
+
+    def __init__(self):
+        self.raw_schedule = []
+        self.precooked_schedule = []
+        self.winter_term = object
+        # self.summer_term = SummerTerm(date(2017, 2, 27), date(2017, 6, 21), date(2017, 4, 14), date(2017, 4, 18))
+        # self.groups_table = Group.get_groups(self.raw_schedule)
+
+    #     TODO: implement connection to db
+
+    def import_ics(self, file_name):
+        self.raw_schedule = FileHandler.read_csv_file(file_name, InputConverter.__HEADERS__)
+
+
