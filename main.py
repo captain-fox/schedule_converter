c8546487ce24ae6e6dddb2fad9efc21acbf1229a
Stanislaw Reduta <stanislaw.reduta@gmail.com>
2017-10-30 20:20:53 +0100

    Routine changes in main for testing.

--- main.py
+++ main.py
@@ -11,28 +11,32 @@ from DatabaseHandler import *
 # Facade between GUI and back-end
 def facade():
 
-    filename = '17l.txt'
+    # filename = '17l.txt'
+    filename = '18zdarz.txt'
     schedule = FileHandler.read_csv_file(filename, InputConverter.__HEADERS__)
 
-    term = SummerTerm(date(2017, 2, 27), date(2017, 6, 21), date(2017, 4, 14), date(2017, 4, 18))
+    # term = SummerTerm(date(2017, 2, 27), date(2017, 6, 21), date(2017, 4, 14), date(2017, 4, 18))
+    term = SummerTerm(date(2017, 9, 28), date(2018, 1, 23), date(2017, 12, 22), date(2018, 1, 2))
     groups = Group.get_groups(schedule)
     print(groups)
     # db = Event.collect_events_for_group(schedule, '6I bl ISA')
     # db = Event.collect_events_for_group(schedule, '6I')
-    db = Event.collect_events_for_group(schedule, '6I IO-2')
+    # db = Event.collect_events_for_group(schedule, '6I IO-2')
+    db = Event.collect_events_for_group(schedule, '7I TM1')
 
     # # #
 
     drb = DatabaseBridge()
-    drb.import_ics('17l.txt')
+    drb.import_ics('18zdarz.txt')
 
     # # #
 
     # outputfile = '6I bl ISA'
-    outputfile = '6I IO-2'
+    # outputfile = '6I IO-2'
+    outputfile = '7I TM1'
     # outputfile = '6I bl TMB'
     # outputfile = '6I'
-    outputfile = None
+    # outputfile = None
     if outputfile is None:
         strategy = ConsoleLogger.create_ics
         outputfile = sys.stdout
